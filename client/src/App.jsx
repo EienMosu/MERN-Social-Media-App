80f8645339333f8bea37a76a264e09c5a1b2a096
EienMosu <82905592+EienMosu@users.noreply.github.com>
2021-12-04 16:31:03 +0100

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HomePage from "./pages/HomePage";
+
+const App = () => {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/pages/HomePage/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container } from './HomePage.styles'
+
+const HomePage = () => {
+    return (
+        <Container>
+            homepage
+            
+        </Container>
+    )
+}
+
+export default HomePage
