6647eeed9539ce805910c6203cbc1effb5e39601
EienMosu <82905592+EienMosu@users.noreply.github.com>
2021-12-18 09:23:33 +0100

    Add files via upload

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 // Redux
 import { useSelector } from "react-redux";
 // React Router
@@ -10,13 +10,7 @@ import Register from "./pages/Register";
 import UserPage from "./pages/UserPage";
 
 const App = () => {
-  // const [user, setUser] = useState(null);
   const user = useSelector((state) => state.user.currentUser);
-
-  // useEffect(() => {
-  //   currentUser && setUser(currentUser.username);
-  //   console.log(user)
-  // }, [currentUser]);
 
   return (
     <BrowserRouter>
